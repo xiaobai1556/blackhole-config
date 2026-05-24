b74a3ffa05bce6d99273268aac2ff2ec6b3ad36f
xiaobai1556 <627785416@qq.com>
2026-05-24 13:27:44 +0800

    Update manifest.lua

--- blackhole-config/manifest.lua
+++ blackhole-config/manifest.lua
@@ -1,19 +1,15 @@
 return {
-  version = "1.0.1",
+  version  = "1.0.2",
+  lc_md    = [[
+![banner](lc_banner.png)
 
-  -- 图片测试：把图片 URL 写进左侧面板 Markdown
-  lc_md = [[
-# Black Hole V1
-
-![图片](https://cdn.jsdelivr.net/gh/xiaobai1556/blackhole-config@main/blackhole-config/images/lc_banner.png)
-## 快速检查
-
-1. 先在「基础设置 / 模型选择」确认模型文件已自动识别。
-2. 帧率为 0 时检查 Camera、Model、Detect 三项。
-3. 鼠标不动时依次确认 CDK、功能开关、安全中心、设备状态。
+你的左面板文字内容...
 ]],
+  rc_md    = [[
+![banner](rc_banner.png)
 
-  rc_md = "",
-  config = {},
-  hot_update = {}
+你的右面板文字内容...
+]],
+  config     = {},
+  hot_update = {},
 }
