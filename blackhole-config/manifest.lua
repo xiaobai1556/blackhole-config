e2f735d6ff9fad87717be247b364cd97273ffd6a
xiaobai1556 <627785416@qq.com>
2026-05-24 13:41:03 +0800

    Update manifest.lua

--- blackhole-config/manifest.lua
+++ blackhole-config/manifest.lua
@@ -1,12 +1,12 @@
 return {
   version  = "1.0.2",
   lc_md    = [[
-![banner](lc_banner.png)
+![banner](lc_banner.b64)
 
 你的左面板文字内容...
 ]],
   rc_md    = [[
-![banner](rc_banner.png)
+![banner](rc_banner.b64)
 
 你的右面板文字内容...
 ]],
